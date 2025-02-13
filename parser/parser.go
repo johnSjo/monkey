e1bf4c6977649eaef5575215a2e2b32d3a171911
johnSjo <john.sjostrom79@gmail.com>
2025-02-13 19:58:19 +0100

    in ch 2.4 page 51 added test for parser

--- parser/parser.go
+++ parser/parser.go
@@ -16,9 +16,17 @@ type Parser struct {
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
+	p.nextToken()
+	p.nextToken()
+
+	return p
 }
 
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
 }
