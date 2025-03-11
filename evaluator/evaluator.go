33ff700db8212d0418a8d1b57afee0f7e4fc9d31
johnSjo <john.sjostrom79@gmail.com>
2025-03-11 07:15:59 +0100

    end ch 3.5 adding the last bool eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -57,6 +57,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
@@ -75,6 +79,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Interger{Value: leftVal * rightVal}
 	case "/":
 		return &object.Interger{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
